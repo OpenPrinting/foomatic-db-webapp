cb4cc00c0113a84a2987834f8e6c2fa6cf5f4c2d
dlopez <>
2009-10-14 22:41:56 +0000

    build env config files

---
+++ inc/siteconf-PROD.php
@@ -0,0 +1,35 @@
+<?php
+
+class SiteConfig {
+		
+	public $htmlTitle = '%s | OpenPrinting - The Linux Foundation';
+		// %s will be replaced with the page title as set per page
+		
+	public $baseURL = '/'; // with tailing slash, from document root
+
+	public $dbUser = 'opuser';
+	public $dbPass = 'Goose5ai';
+	public $db = 'openprinting';
+	public $dbServer = 'db.linuxfoundation.org';
+	
+	/*public $dbUser = 'root';
+	public $dbPass = 'administrator';
+	public $db = 'openprinting';
+	public $dbServer = 'localhost';*/
+	
+	/*public $ldapServer = '140.211.169.120';
+	public $ldapServer2 = false;
+	public $ldapBaseDN = 'dc=lf,dc=org';
+	public $ldapUserBaseDN = 'ou=Users,dc=lf,dc=org';
+	public $ldapUsernameField = 'uid';
+	public $ldapMailField = 'mail';*/
+	
+	public $ldapServer = 'ldap1.linux-foundation.org';
+	public $ldapServer2 = 'ldap2.linux-foundation.org';
+	public $ldapBaseDN = 'dc=freestandards,dc=org';
+	public $ldapUserBaseDN = 'ou=Users,dc=freestandards,dc=org';
+	public $ldapUsernameField = 'uid';
+	public $ldapMailField = 'mail';	
+}
+
+?>
