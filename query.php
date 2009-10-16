a9fe50c646f3b2f9aeed5f8d999667b5ef58cdc9
till <>
2009-10-16 17:12:23 +0000

    Added PHP wrapper for the web query API for printer setup tools.

---
+++ query.php
@@ -0,0 +1,28 @@
+<?php
+
+// PHP wrapper for running the web query API for printer setup tools
+// http://www.linuxfoundation.org/en/OpenPrinting/Database/Query
+// The output of the called program "query" should not be modified, to
+// keep the web query API compatible to the former site.
+// PHP Code for access statistics, logging, ... can be added though.
+
+// To be compatible with the former query.cgi the line
+//   RewriteRule ^query.cgi/?$ query.php               [L]
+// needs to be added to .htaccess
+
+if ($_GET['format'] == "xml") {
+  header("Content-Type: text/xml; name=query.xml; charset=UTF-8");
+  header("Content-Disposition: inline; filename=\"query.xml\"");
+} else {
+  header("Content-Type: text/plain; name=query.txt; charset=UTF-8");
+  header("Content-Disposition: inline; filename=\"query.txt\"");
+}
+
+$querycmdline = "cd foomatic; ./query";
+foreach($_GET as $k => $v) {
+  $querycmdline .= " $k='$v'";
+}
+
+passthru($querycmdline);
+
+?>
