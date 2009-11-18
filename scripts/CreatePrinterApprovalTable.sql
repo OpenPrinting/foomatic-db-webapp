6baa8e26154747152a50cf1233021cf0626de31b
till <>
2009-11-18 16:27:19 +0000

    Corrections.

--- scripts/CreatePrinterApprovalTable.sql
+++ scripts/CreatePrinterApprovalTable.sql
@@ -2,11 +2,11 @@ CREATE TABLE printer_approval
 (
 	id VARCHAR(50) NOT NULL COMMENT '<make>-<model> is the ID format. Spaces are replaced with _',
 	contributor VARCHAR(40),
-	show VARCHAR(40),
+	showentry VARCHAR(40),
 	approved VARCHAR(40),
 	rejected VARCHAR(40),
 	approver VARCHAR(40),
 	comment TEXT,
-	CONSTRAINT pkey PRIMARY KEY(id)
+	CONSTRAINT pkey PRIMARY KEY(id),
 	FOREIGN KEY(id) REFERENCES printer(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
