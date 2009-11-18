f5310f422b5060b02f483f7a2c16132fd4fa2be5
till <>
2009-11-18 17:27:29 +0000

    More corrections.

--- scripts/CreateDriverApprovalTable.sql
+++ scripts/CreateDriverApprovalTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE driver_approval
 (
-	id VARCHAR(50) NOT NULL COMMENT '<make>-<model> is the ID format. Spaces are replaced with _',
+	id VARCHAR(50) NOT NULL,
 	contributor VARCHAR(40),
 	showentry VARCHAR(40),
 	approved VARCHAR(40),
