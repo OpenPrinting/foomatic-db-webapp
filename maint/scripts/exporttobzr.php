cab1aedb7ad62127eb41b45f169ebc0c22995f9b
till <>
2009-11-26 17:42:31 +0000

    Added script for a daily cron job to commit the current MySQL database state into the BZR repositories.

--- maint/scripts/exporttobzr.php
+++ maint/scripts/exporttobzr.php
@@ -8,8 +8,10 @@ require_once("option/option.php");
 include('inc/siteconf.php');
 $CONF = new SiteConfig();
 
-$opts = getopt("fna");
+$opts = getopt("fnad:p:");
 $selection = "all";
+$basedir = "foomatic";
+$ppdsource = false;
 foreach (array_keys($opts) as $opt) switch ($opt) {
   case 'f':
     $selection = "free";
@@ -20,17 +22,26 @@ foreach (array_keys($opts) as $opt) switch ($opt) {
   case 'a':
     $selection = "all";
     break;
+  case 'd':
+    $basedir = $opts['d'];
+    break;
+  case 'p':
+    $ppdsource = $opts['p'];
+    break;
 }
 
 if ($selection == 'nonfree') {
-  $foomatic_dir = "foomatic/foomatic-db-nonfree";
+  $foomatic_dir = "foomatic-db-nonfree";
 } else {
-  $foomatic_dir = "foomatic/foomatic-db";
+  $foomatic_dir = "foomatic-db";
 }
+if ($basedir != "")
+  $foomatic_dir = $basedir . "/" . $foomatic_dir;
 //$foomatic_dir = "../../foomatic-db";
 $driver_dir = $foomatic_dir."/db/source/driver";
 $printer_dir = $foomatic_dir."/db/source/printer";
 $option_dir = $foomatic_dir."/db/source/opt";
+$source_dir = $foomatic_dir."/db/source";
 
 if ($selection == 'nonfree') {
   $nonfree = "driver.nonfreesoftware=1 and ";
@@ -64,6 +75,9 @@ $querydrivers = "select driver.id from " .
   "driver_approval.showentry=1 or " .
   "driver_approval.showentry<=CAST(NOW() AS DATE))));";
 $queryoptions = "select id from options;";
+$queryppdlist = "select driver_printer_assoc.ppd from " .
+  "driver_printer_assoc, driver " .
+  "where " . $nonfree . "driver_printer_assoc.driver_id=driver.id;";
 
 # Connect to MySQL database
 $db = DB::getInstance();
@@ -74,20 +88,20 @@ if ($selection != 'nonfree') {
   $dir = $printer_dir;
   $result = $db->query($queryprinters);
   if ($result == null) {
-    echo "[ERROR] Unable to get list of printers to be exported: ".$db->getError()."\n";
+    fwrite(STDERR, "[ERROR] Unable to get list of printers to be exported: ".$db->getError()."\n");
     exit;
   }
   $dircreated = 0;
   while($row = mysql_fetch_array($result)) {
     $id = $row['id'];
-    print "Exporting printer $id ...\n";
+    fwrite(STDERR, "Exporting printer $id ...\n");
     $printer = new Printer();
     $error = !$printer->loadDB($id);
     if (!$error) {
       $xml = $printer->toXML();
       $filename = "$id.xml";
       if ($dircreated == 0) {
-	mkdir($dir, 0755, true);
+	exec ("mkdir -p $dir");
 	$dircreated = 1;
       }
       $fh = fopen("$dir/$filename", "w");
@@ -105,7 +119,7 @@ if ($selection != 'nonfree') {
   }
 
   if ($error) {
-    print "[FATAL ERROR]: Something went wrong while exporting printer data\n";
+    fwrite(STDERR, "[FATAL ERROR]: Something went wrong while exporting printer data\n");
     exit;
   }
 }
@@ -115,20 +129,20 @@ $error = false;
 $dir = $driver_dir;
 $result = $db->query($querydrivers);
 if ($result == null) {
-  echo "[ERROR] Unable to get list of drivers to be exported: ".$db->getError()."\n";
+  fwrite(STDERR, "[ERROR] Unable to get list of drivers to be exported: ".$db->getError()."\n");
   exit;
 }
 $dircreated = 0;
 while($row = mysql_fetch_array($result)) {
   $id = $row['id'];
-  print "Exporting driver \"$id\" ...\n";
+  fwrite(STDERR, "Exporting driver \"$id\" ...\n");
   $driver = new Driver();
   $error = !$driver->loadDB($id);
   if (!$error) {
     $xml = $driver->toXML();
     $filename = "$id.xml";
     if ($dircreated == 0) {
-      mkdir($dir, 0755, true);
+      exec ("mkdir -p $dir");
       $dircreated = 1;
     }
     $fh = fopen("$dir/$filename", "w");
@@ -146,7 +160,7 @@ while($row = mysql_fetch_array($result)) {
 }
 
 if ($error) {
-  print "[FATAL ERROR]: Something went wrong while exporting driver data\n";
+  fwrite(STDERR, "[FATAL ERROR]: Something went wrong while exporting driver data\n");
   exit;
 }
 
@@ -156,20 +170,20 @@ if ($selection != 'nonfree') {
   $dir = $option_dir;
   $result = $db->query($queryoptions);
   if ($result == null) {
-    echo "[ERROR] Unable to get list of options to be exported: ".$db->getError()."\n";
+    fwrite(STDERR, "[ERROR] Unable to get list of options to be exported: ".$db->getError()."\n");
     exit;
   }
   $dircreated = 0;
   while($row = mysql_fetch_array($result)) {
     $id = $row['id'];
-    print "Exporting option \"$id\" ...\n";
+    fwrite(STDERR, "Exporting option \"$id\" ...\n");
     $option = new Option();
     $error = !$option->loadDB($id);
     if (!$error) {
       $xml = $option->toXML();
       $filename = "$id.xml";
       if ($dircreated == 0) {
-	mkdir($dir, 0755, true);
+	exec ("mkdir -p $dir");
 	$dircreated = 1;
       }
       $fh = fopen("$dir/$filename", "w");
@@ -187,7 +201,31 @@ if ($selection != 'nonfree') {
   }
 
   if ($error) {
-    print "[FATAL ERROR]: Something went wrong while exporting option data\n";
+    fwrite(STDERR, "[FATAL ERROR]: Something went wrong while exporting option data\n");
+    exit;
+  }
+}
+
+if ($ppdsource) {
+  # export the printer data
+  $error = false;
+  $dir = $ppdsource . "/db/source";
+  $result = $db->query($queryppdlist);
+  if ($result == null) {
+    fwrite(STDERR, "[ERROR] Unable to get list of PPDs to be copied: ".$db->getError()."\n");
+    exit;
+  }
+  while($row = mysql_fetch_array($result)) {
+    $file = $row['ppd'];
+    if ($file == NULL) continue;
+    $destdir = $source_dir . "/" . dirname($file);
+    fwrite(STDERR, "Copying $file ...\n");
+    exec ("mkdir -p $destdir");
+    exec ("cp -f $dir/$file $destdir");
+  }
+
+  if ($error) {
+    fwrite(STDERR, "[FATAL ERROR]: Something went wrong while copying PPD files\n");
     exit;
   }
 }
