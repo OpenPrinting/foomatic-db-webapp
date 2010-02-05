a315adc32e1be111b697c545b1dd25c2787dade1
till <>
2010-02-05 09:47:03 +0000

    Driver list page: Do not list drivers which are not yet released or not approved.

--- driver_list.php
+++ driver_list.php
@@ -31,25 +31,34 @@ $PAGE->addBreadCrumb('Drivers',$CONF->baseURL.'drivers/');
 if($SESSION->checkPermission('driver_upload')) $SMARTY->assign("UPLOAD_ALLOWED",1);
 
 $res = $DB->query("
-	SELECT id, dr.name AS name, execution, shortdescription, printerCount,
-	       driver_package.name AS package FROM
-	       (SELECT id, name, execution, shortdescription,
-		pj.count as printerCount
-		FROM driver 
-		LEFT JOIN 
-			(SELECT driver_id, count(printer_id) as count 
-			 FROM driver_printer_assoc
-			 GROUP BY driver_id)
-			 AS pj
-			 ON pj.driver_id = driver.id) AS dr
-	LEFT JOIN driver_package 
-	ON dr.id=driver_package.driver_id
-	ORDER BY dr.name
+	SELECT dra.id AS id, name, execution, shortdescription, printerCount,
+	       package FROM
+	       (SELECT id, dr.name AS name, execution, shortdescription,
+		       printerCount, driver_package.name AS package FROM
+		       (SELECT id, name, execution, shortdescription,
+			       pj.count as printerCount FROM
+		       driver 
+		       LEFT JOIN 
+		       (SELECT driver_id, count(printer_id) as count 
+			FROM driver_printer_assoc
+			GROUP BY driver_id) AS pj
+		       ON pj.driver_id = driver.id) AS dr
+	       LEFT JOIN driver_package 
+	       ON dr.id=driver_package.driver_id) AS dra
+	LEFT JOIN driver_approval
+	ON dra.id=driver_approval.id
+	WHERE (driver_approval.id IS NULL OR
+	(driver_approval.approved IS NOT NULL AND
+	driver_approval.approved!=0 AND driver_approval.approved!='' AND
+	(driver_approval.rejected IS NULL OR driver_approval.rejected=0 OR
+	driver_approval.rejected='') AND
+	(driver_approval.showentry IS NULL OR
+	driver_approval.showentry='' OR
+	driver_approval.showentry=1 OR
+	driver_approval.showentry<=CAST(NOW() AS DATE))))
+	ORDER BY name
 	");
 $r = $res->toArray('id');
-/*
-SELECT id, dr.name, execution, shortdescription, printerCount, driver_package.name FROM (SELECT id, name, execution, shortdescription, pj.count as printerCount FROM driver LEFT JOIN (SELECT driver_id, count(printer_id) as count FROM driver_printer_assoc GROUP BY driver_id) AS pj ON pj.driver_id = driver.id) AS dr LEFT JOIN driver_package ON dr.id=driver_package.driver_id ORDER BY dr.name;
-*/
 
 $SMARTY->assign("drivers",$r);
 
