77be00840b956a44aab00c014276119d41e96133
till <>
2010-02-05 16:16:02 +0000

    Driver license page: Do not show the license if the driver is not approved, not yet released, or rejected.

--- driver_license.php
+++ driver_license.php
@@ -25,9 +25,24 @@ $PAGE->addBreadCrumb($_GET['driver'],
 		     $CONF->baseURL.'driver/'.$_GET['driver']."/");	
 $PAGE->addBreadCrumb('License');	
 
-// Load driver's license info
-$res = $DB->query("SELECT name, license, licensetext, licenselink FROM driver WHERE id = '?'", $_GET['driver']);
-$driver = $res->getRow();
+// Check if the driver is already accepted, released, and not rejected
+$res = $DB->query("
+    SELECT id FROM driver_approval
+    WHERE id='" . $_GET['driver'] . "' AND
+    (approved IS NULL OR approved=0 OR approved='' OR
+     (rejected IS NOT NULL AND rejected!=0 AND rejected!='') OR
+     (showentry IS NOT NULL AND showentry!='' AND showentry!=1 AND
+      showentry>CAST(NOW() AS DATE)))
+");
+$row = $res->getRow();
+if (count($row) == 0) {
+    // Load driver's license info (Load only if the driver is accepted, not
+    // rejected, and released)
+    $res = $DB->query("SELECT name, license, licensetext, licenselink FROM driver WHERE id = '?'", $_GET['driver']);
+    $driver = $res->getRow();
+} else {
+    $driver = null;
+}
 $SMARTY->assign('driver', $driver);
 
 $SMARTY->display('drivers/license.tpl');
