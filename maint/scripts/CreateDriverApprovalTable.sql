5c00d57ecfd7338575fedb08fc2ace8cb24c6d66
till <>
2010-02-17 20:30:01 +0000

    Use DATETIME data types for ..._approval tables, added field for sub,ission date.

--- maint/scripts/CreateDriverApprovalTable.sql
+++ maint/scripts/CreateDriverApprovalTable.sql
@@ -2,9 +2,10 @@ CREATE TABLE driver_approval
 (
 	id VARCHAR(50) NOT NULL,
 	contributor VARCHAR(40),
-	showentry VARCHAR(40),
-	approved VARCHAR(40),
-	rejected VARCHAR(40),
+	submitted DATETIME,
+	showentry DATETIME,
+	approved DATETIME,
+	rejected DATETIME,
 	approver VARCHAR(40),
 	comment TEXT,
 	CONSTRAINT pkey PRIMARY KEY(id),
