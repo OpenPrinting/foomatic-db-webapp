eede0af3a991678e7d99ef7ebd77d8407b1d1d0d
Anton Kirilenko <anton.kirilenko.rosa@gmail.com>
2013-07-30 20:47:33 -0400

    Added a script to setup an empty database

---
+++ maint/scripts/setupdb.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# This will return a single line with all necessary params in a 'key=value' form
+CONF=`cd ../../foomatic &&  php ../dump_mysql_config.php | sed 's/: /=/g'`
+# This will export 'server', 'user', 'password' and 'database' variables
+export $CONF
+
+DBPARAMS="--password=$password -u $user -h $server"
+
+mysql $DBPARAMS -e "DROP DATABASE IF EXISTS $database"
+mysql $DBPARAMS -e "CREATE DATABASE $database"
+
+# Preserver correct table order so foreign key can be created!
+for i in \
+    CreateDriverTable \
+    CreateDriverApprovalTable \
+    CreateDriverDependency \
+    CreateDriverPackagesTable \
+    CreateDriverPrinterAssociationTable \
+    CreateDriverPrinterAssociationTranslationTable \
+    CreateDriverSupportContactsTable \
+    CreateDriverSupportContactsTranslationTable \
+    CreateDriverTranslationTable \
+    CreateMarginsTable \
+    CreateOptionsTable \
+    CreateOptionChoicesTable \
+    CreateOptionChoicesTranslationTable \
+    CreateOptionConstraintsTable \
+    CreateOptionTranslationTable \
+    CreatePrinterTable \
+    CreatePrinterApprovalTable \
+    CreatePrinterTranslationTable \
+    CreateWebNotifications \
+    CreateWebPermissions \
+    CreateWebRoles \
+    CreateWebRoleUserAssign \
+    createweb_user \
+    CreateWebRolePrivAssign \
+    UpdateWebPermissions
+do
+    echo Processing ${i}.sql
+    mysql $DBPARAMS $database < ${i}.sql
+done
+
