55293a241a1542eec596fca491772c5d87ccb020
Till Kamppeter <till.kamppeter@gmail.com>
2016-09-30 22:17:20 -0300

    In inc/db.pm switch back from prepared MySQL queries to simply substituting the question marks by the parameters as in some cases prepared queries did not work.

--- inc/db.php
+++ inc/db.php
@@ -112,7 +112,8 @@ class DB {
 	}
 	
 	function query($query, $arguments = array()) {
-		
+
+		/*
 		$this->lastStatement = $this->connection->prepare($query);
 		
 		if (!is_array($arguments)) {
@@ -130,7 +131,7 @@ class DB {
 		}
 		
 		return $result;
-		/*
+		*/
 		$args  = func_get_args();
 		$query = array_shift($args);		
 		$query = str_replace("?", "%s", $query);
@@ -155,7 +156,6 @@ class DB {
 			return false;
 		} else 
 			return $res;
-		*/
 	}	
 }
 
