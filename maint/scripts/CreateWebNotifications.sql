83d79d04dfab08e5b35290b41f4f6ad85d8ad89d
Violet Kurtz <vikurtz@osuosl.org>
2022-04-04 14:12:05 -0700

    Removed account-related database tables

--- maint/scripts/CreateWebNotifications.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `web_notifications` (
-  `web_user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL DEFAULT '',
-  `printer_queue` tinyint(1) NOT NULL DEFAULT '0',
-  `printer_noqueue` tinyint(1) NOT NULL DEFAULT '0',
-  `driver_queue` tinyint(1) NOT NULL DEFAULT '0',
-  `driver_noqueue` tinyint(1) NOT NULL,
-  PRIMARY KEY (`web_user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
