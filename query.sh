fc03c51fd743d170acc2c78ff35979310636c95a
Violet Kurtz <vikurtz@osuosl.org>
2022-08-29 12:03:18 -0700

    Update query.sh to support split printer app listings

--- query.sh
+++ query.sh
@@ -15,7 +15,15 @@ for ARG in "$@"; do
   ARGS+=(-o "$KEY=\"$VALUE\"")
 done
 
-while read -r PAPP; do
+while read -r PAPP_LINE; do
+  PAPP=""
+  if [ "$(echo "$PAPP_LINE" | awk '{ print NF }')" -gt 1 ]
+  then
+    PAPP="$(echo "$PAPP_LINE" | awk '{ print $2 }')"
+  else
+    PAPP=$PAPP_LINE
+  fi
+
   OUTPUT=$("$SNAP_DIR$PAPP" drivers "${ARGS[@]}")
   echo "$PAPP: $OUTPUT"
 done < printer-apps.txt
