3f22113a1168c1b606193c4ac05df527b5e399e9
Violet Kurtz <vikurtz@osuosl.org>
2022-09-15 14:15:49 -0700

    Added printer app caching

--- query.sh
+++ query.sh
@@ -7,23 +7,39 @@ if [ ! -f printer-apps.txt ]; then
   exit
 fi
 
+if [ ! -d cache ]; then
+  mkdir cache
+fi
+
 ARGS=()
+DEVICE_ID=""
 
 for ARG in "$@"; do
   KEY=${ARG%%=*}
   VALUE=${ARG#*=}
   ARGS+=(-o "$KEY=\"$VALUE\"")
+  
+  if [ "$KEY" = "device-id" ]; then
+    DEVICE_ID="$VALUE"
+  fi
 done
 
-while read -r PAPP_LINE; do
-  PAPP=""
-  if [ "$(echo "$PAPP_LINE" | awk '{ print NF }')" -gt 1 ]
-  then
-    PAPP="$(echo "$PAPP_LINE" | awk '{ print $2 }')"
-  else
-    PAPP=$PAPP_LINE
-  fi
+# Fall back to snap if no cache
+if [ ! -f "cache/$DEVICE_ID.out" ]; then
+  while read -r PAPP_LINE; do
+    PAPP=""
+    if [ "$(echo "$PAPP_LINE" | awk '{ print NF }')" -gt 1 ]
+    then
+      PAPP="$(echo "$PAPP_LINE" | awk '{ print $2 }')"
+    else
+      PAPP=$PAPP_LINE
+    fi
 
-  OUTPUT=$("$SNAP_DIR$PAPP" drivers "${ARGS[@]}")
-  echo "$PAPP: $OUTPUT"
-done < printer-apps.txt
+    OUTPUT=$("$SNAP_DIR$PAPP" drivers "${ARGS[@]}")
+    echo "$PAPP: $OUTPUT" > "cache/$DEVICE_ID.out"
+  done < printer-apps.txt
+fi
+
+# Write out cache contents
+cat "cache/$DEVICE_ID.out"
+
